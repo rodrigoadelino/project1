1bd822a9638fb41a879f494ce56c3c915ee86812
Rodrigo Bonfim <rodrigoadelino@outlook.com>
2024-07-10 06:51:46 -0300

    aula23_secao5

---
+++ aulas/aula23_secao5.py
@@ -0,0 +1,30 @@
+## Versão assíncrona do converter
+
+# No arquivo converter.py iremos importa a biblioteca
+import aiohttp
+
+
+# incluindo a função assícrona abaixo
+
+
+
+def async_converter(from_currency: str, to_corrency:str, price: float):
+    url = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={from_currency}&to_currency={to_corrency}&apikey={ALPHAVANTAGE_APIKEY}'
+
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url=url) as response:
+                data = await response.json()
+                
+    except Exception as error:
+        raise HTTPException(status_code=400, detail=error)
+    
+    data = response.json()
+
+    
+    if "Realtime Currency Exchange Rate" not in data:
+        raise HTTPException(status_code=400, detail='Realtime Currency Exchange Rate" not in response')
+    
+    exchange_rate = float(data['Realtime Currency Exchange Rate']['5. Exchange Rate'])
+
+    return price * exchange_rate
--- converter.py
+++ converter.py
@@ -1,5 +1,6 @@
 import requests
 from os import getenv
+import aiohttp
 from fastapi import HTTPException
 
 
@@ -15,6 +16,25 @@ def sync_converter(from_currency: str, to_corrency:str, price: float):
     
     data = response.json()
 
+    
+    if "Realtime Currency Exchange Rate" not in data:
+        raise HTTPException(status_code=400, detail='Realtime Currency Exchange Rate" not in response')
+    
+    exchange_rate = float(data['Realtime Currency Exchange Rate']['5. Exchange Rate'])
+
+    return price * exchange_rate
+
+
+def async_converter(from_currency: str, to_corrency:str, price: float):
+    url = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={from_currency}&to_currency={to_corrency}&apikey={ALPHAVANTAGE_APIKEY}'
+
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url=url) as response:
+                data = await response.json()
+                
+    except Exception as error:
+        raise HTTPException(status_code=400, detail=error)
     
     if "Realtime Currency Exchange Rate" not in data:
         raise HTTPException(status_code=400, detail='Realtime Currency Exchange Rate" not in response')
